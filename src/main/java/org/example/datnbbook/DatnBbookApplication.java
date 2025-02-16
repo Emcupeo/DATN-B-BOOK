222db740bab561982a505336a356079cfcab4fac
Emcupeo <Emcupeo123@gmail.com>
2025-02-17 00:01:48 +0700

    Initial commit

---
+++ src/main/java/org/example/datnbbook/DatnBbookApplication.java
@@ -0,0 +1,13 @@
+package org.example.datnbbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatnBbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatnBbookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/datnbbook/DatnBbookApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.datnbbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatnBbookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
